c7f2f334d4e0f462181fa290dcadeb9332cd97e5
kokon322 <chornuy322@gmail.com>
2021-03-22 16:37:45 +0100

    start

--- server/app.js
+++ server/app.js
@@ -1,17 +1,15 @@
 const path = require('path');
 require('dotenv').config({path: path.join(process.cwd(), 'pizza_crud', 'server', '.env')});
 const express = require('express');
-const mongoose = require('mongoose');
+
 
 const apiRouter = require('./routers/api.router');
-const {PORT, MONGO_URL} = require('./configs/config');
+const {PORT} = require('./configs/config');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-_ConnectToDB();
 
 app.use('/', apiRouter);
 
@@ -22,13 +20,3 @@ app.use('*', (err, req, res, next) => {
 app.listen(PORT, () => {
     console.log(`${PORT} is work`);
 });
-
-function _ConnectToDB() {
-    mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-
-    const {connection} = mongoose;
-
-    connection.on('error', err => {
-        console.log(err);
-    });
-}
